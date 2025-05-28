2654efa4eacb64332e3beb99f9c5b87707fe953e
BOSCO0109 <boscodon321@gmail.com>
2025-05-28 17:52:39 +0530

    Update README.py
    .

--- README.py
+++ README.py
@@ -1,2 +1,10 @@
 # data-cleaning-mysql-upload
-A mini data engineering project focusing on data cleaning using Python (Pandas) and uploading the processed data into a MySQL database.
+# A mini data engineering project focusing on data cleaning using Python (Pandas) and uploading the processed data into a MySQL database.
+
+from sqlalchemy import create_engine
+import ast
+import pandas as ps
+
+A = ps.read_json(r"C:\Users\6327105\Downloads\project_2.json")
+A.to_csv('rexx.csv')
+print('success')
